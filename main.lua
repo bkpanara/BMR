8a679e24b18a65751d9f5ad96259865e2b8b4a42
Bhavin <sptechnolab.bhavin@gmail.com>
2014-01-02 11:40:57 +0530

    test..

--- main.lua
+++ main.lua
@@ -1,8 +1,6 @@
 
-<<<<<<< HEAD
+
 
 local txt = display.newText("hello from bmr....." , 100 , 100 , native.systemFont , 20)
-=======
-local txt = display.newText("hello from bmr....." , 100 , 100 , native.systemFont , 20)
 txt:setTextColor(100,100,100)
->>>>>>> f121299e6df60ef6ea1e8316cbb297c4fe454f68
+
