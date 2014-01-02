f121299e6df60ef6ea1e8316cbb297c4fe454f68
bkpanara <sptechnolab.bhavin@gmail.com>
2014-01-02 11:38:34 +0530

    set color

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
 
-
-display.newText("hello from bmr....." , 100 , 100 , native.systemFont , 20)
+local txt = display.newText("hello from bmr....." , 100 , 100 , native.systemFont , 20)
+txt:setTextColor(100,100,100)
